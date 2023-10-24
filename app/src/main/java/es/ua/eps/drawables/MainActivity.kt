73111bc64c24caf67ea972aa1663b5151d9357b4
di-saldana <dsl42@alu.ua.es>
2023-10-24 15:00:13 +0200

    Notificaciones - Ejercicio 3

--- app/src/main/java/es/ua/eps/drawables/MainActivity.kt
+++ app/src/main/java/es/ua/eps/drawables/MainActivity.kt
@@ -1,8 +1,14 @@
 package es.ua.eps.drawables
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.widget.Button
 import android.widget.SeekBar
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +17,11 @@ class MainActivity : AppCompatActivity() {
 
         val grafica = findViewById<Grafica>(R.id.grafica)
         val seekBar = findViewById<SeekBar>(R.id.seekGrafica)
+
+        val colorButton = findViewById<Button>(R.id.b1)
+        val tamanoButton = findViewById<Button>(R.id.b2)
+        val p1 = findViewById<TextView>(R.id.p1)
+        val p2 = findViewById<TextView>(R.id.p2)
 
         seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
@@ -21,5 +32,71 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
+
+        colorButton.setOnClickListener { showDialogoColor(p1, p2) }
+        tamanoButton.setOnClickListener { showDialogoTamano(p1, p2) }
+    }
+
+    private fun showDialogoTamano(p1: TextView, p2: TextView) {
+        val tamanos = arrayOf("Pequeño", "Normal", "Grande")
+
+        AlertDialog.Builder(this)
+            .setTitle("Selecciona el tamaño")
+            .setItems(tamanos) { dialog, which ->
+                Log.i("Dialogos", "Opción elegida: " + tamanos[which])
+
+                when (which) {
+                    0 -> {
+                        p1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8F)
+                        p2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 8F)
+                    }
+                    1 -> {
+                        p1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
+                        p2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12F)
+                    }
+                    2 -> {
+                        p1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
+                        p2.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20F)
+                    }
+                }
+            }
+            .setNegativeButton(R.string.cancelar, null)
+            .show()
+    }
+
+    private fun showDialogoColor(p1: TextView, p2: TextView) {
+        val colores = arrayOf("Blanco y Negro", "Negro y Blanco", "Negro y Verde")
+
+        AlertDialog.Builder(this)
+            .setTitle("Selecciona los colores")
+            .setItems(colores) { dialog, which ->
+                Log.i("Dialogos", "Opción elegida: " + colores[which])
+
+                when (which) {
+                    0 -> {
+                        p1.setBackgroundColor(Color.WHITE)
+                        p1.setTextColor(Color.BLACK)
+
+                        p2.setBackgroundColor(Color.WHITE)
+                        p2.setTextColor(Color.BLACK)
+                    }
+                    1 -> {
+                        p1.setBackgroundColor(Color.BLACK)
+                        p1.setTextColor(Color.WHITE)
+
+                        p2.setBackgroundColor(Color.BLACK)
+                        p2.setTextColor(Color.WHITE)
+                    }
+                    2 -> {
+                        p1.setBackgroundColor(Color.BLACK)
+                        p1.setTextColor(Color.GREEN)
+
+                        p2.setBackgroundColor(Color.BLACK)
+                        p2.setTextColor(Color.GREEN)
+                    }
+                }
+            }
+            .setNegativeButton(R.string.cancelar, null)
+            .show()
     }
 }
