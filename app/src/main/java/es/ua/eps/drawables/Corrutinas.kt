0eb3f415d94f3397d1637d3ec341f9a7647fc5ce
di-saldana <dsl42@alu.ua.es>
2023-10-26 18:52:24 +0200

    Hilos de ejecución - Corrutinas

--- app/src/main/java/es/ua/eps/drawables/Corrutinas.kt
+++ app/src/main/java/es/ua/eps/drawables/Corrutinas.kt
@@ -2,10 +2,26 @@ package es.ua.eps.drawables
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class Corrutinas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_corrutinas)
+
+        val tvCrono = findViewById<TextView>(R.id.tvCrono)
+        var t = 10
+
+        GlobalScope.launch(Dispatchers.IO) {
+            do {
+                tvCrono.text = "Contador: $t"
+                Thread.sleep(1000)
+                t--
+            } while (t > 0)
+            tvCrono.text = "Contador terminado"
+        }
     }
 }
