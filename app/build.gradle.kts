90940f7940a8dbcbb51633f2aa60fba02aa54778
di-saldana <dsl42@alu.ua.es>
2023-10-26 18:55:42 +0200

    Updated Coroutines in Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.coordinatorlayout:coordinatorlayout:1.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
 }
